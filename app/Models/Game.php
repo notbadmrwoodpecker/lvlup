eaceb39b385c5ffff2c6b4dcc1a0538a3e688a9d
Christian <christian.arp@posteo.de>
2024-10-28 11:29:49 +0100

    feat: adding and extending new scopes for Game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -18,11 +18,33 @@ class Game extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    /**
+     * Scope which finds the most popular games based on the
+     * amount of reviews (optionally within a specific date range).
+     * @param Builder $query
+     * @param string|null $from from date for created_at of reviews
+     * @param string|null $to to date for created_at of reviews
+     *
+     * @return Builder extended query
+     */
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder {
+        return $query->withCount(['reviews' => fn(Builder $q) => $q->filterByDateRange($from, $to)])->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+    /**
+     * Scope which finds the highest rated games based on the
+     * average rating of reviews (optionally within a specific date range).
+     * @param Builder $query
+     * @param string|null $from from date for created_at of reviews
+     * @param string|null $to to date for created_at of reviews
+     *
+     * @return Builder extended query
+     */
+    public function scopeHighestRated(Builder $query, string $from = null, string $to = null): Builder {
+        return $query->withAvg(['reviews' => fn(Builder $q) => $q->filterByDateRange($from, $to)], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinimumReviews(Builder $query, int $minimumAmountOfReviews): Builder {
+        return $query->having('reviews_count', '>=', $minimumAmountOfReviews);
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -11,5 +12,27 @@ class Review extends Model
 
     public function game() {
         return $this->belongsTo(Game::class);
+    }
+
+    /**
+     * Scope to filter the reviews by a specific (optional) date range.
+     * @param Builder $query
+     * @param string|null $from from date for created_at of reviews
+     * @param string|null $to to date for created_at of reviews
+     */
+    private function scopeFilterByDateRange(Builder $query, string $from, string $to) {
+        if (!$from && !$to) {
+            return;
+        }
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+            return;
+        }
+        if (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+            return;
+        }
+
+        $query->whereBetween('created_at', [$from, $to]);
     }
 }
